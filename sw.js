f9eb285115c90991bd7936dc90d53f405b8b2c85
Victrricem <35552149+victricem@users.noreply.github.com>
2026-01-16 15:40:31 +0800

    sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+// sw.js 內容
+const CACHE_NAME = 'ff14-hunt-v3';
+const urlsToCache = [
+  './',
+  './index.html'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
